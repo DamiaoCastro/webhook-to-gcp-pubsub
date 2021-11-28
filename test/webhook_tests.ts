012f1344b998805f0aea1a807abe1b73bd282bf5
Damião Castro <damiao.c@hotmail.com>
2021-11-28 15:15:10 +0100

    passing unit tests for default response body

--- test/webhook_tests.ts
+++ test/webhook_tests.ts
@@ -1,7 +1,7 @@
 import { describe } from "mocha";
 import { Webhook } from "../src/webhook";
 import { mockRequest, mockResponse } from "mock-req-res";
-const assert = require('assert');
+import { strict as assert } from 'assert';
 
 describe('test default response when NOT defined in the environment variables', function () {
 
@@ -18,11 +18,47 @@ describe('test default response when NOT defined in the environment variables', 
     //act
     webhook.handler(request, response);
 
-    console.info(response);
+    //assert
+    it('statusCode', function () {
+        assert.equal(response.status.calledOnce, true);
+        assert.equal(response.status.calledOnceWith(200), true);
+    });
+    
+    it('response body', function () {
+        assert.equal(response.send.calledOnce, true);
+        assert.equal(response.send.calledOnceWith("OK"), true);
+    });
+
+});
+
+
+describe('test default response when defined in the environment variables', function () {
+
+    //arrange
+    const defaultResponse = "[default response]";
+
+    const environmentVariables = {
+        "test": "123",
+        "DEFAULT_RESPONSE": defaultResponse,
+    };
+
+    const webhook = new Webhook(environmentVariables);
+
+    const request = mockRequest({ body: "{ some dummy body }" })
+    const response = mockResponse();
+
+    //act
+    webhook.handler(request, response);
 
     //assert
     it('statusCode', function () {
-        assert.equal(response.statusCode, 200);
+        assert.equal(response.status.calledOnce, true);
+        assert.equal(response.status.calledOnceWith(200), true);
+    });
+    
+    it('response body', function () {
+        assert.equal(response.send.calledOnce, true);
+        assert.equal(response.send.calledOnceWith(defaultResponse), true);
     });
 
 });
