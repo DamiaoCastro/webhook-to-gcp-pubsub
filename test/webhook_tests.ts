80d6c8a0dadad452843739200084d4b578da967f
Damião Castro <damiao.c@hotmail.com>
2021-12-07 13:00:51 +0100

    found a way to test

--- test/webhook_tests.ts
+++ test/webhook_tests.ts
@@ -1,17 +1,20 @@
 import { describe } from "mocha";
 import { Webhook } from "../src/webhook";
 import { strict as assert } from 'assert';
-// import { IncomingMessage, ServerResponse } from "node:http";
+import { IncomingMessage, ServerResponse } from "node:http";
 // import { sinon } from 'sinon';
 const http = require('http');
-const sinon = require('sinon');
+// const sinon = require('sinon');
+import * as sinon from "ts-sinon";
 
-const publishMessageAsync = sinon.fake();
+// const publishMessageAsync = sinon.;
 
-const publisher: IPubsubPublisher = {
-    publishMessageAsync: publishMessageAsync,
-    checkPublishPermissionsAsync: function (): Promise<void> { throw new Error("Function not implemented."); }
-};
+const publisher: IPubsubPublisher = sinon.stubInterface<IPubsubPublisher>();
+
+// const publisher: IPubsubPublisher = {
+//     publishMessageAsync: publishMessageAsync,
+//     checkPublishPermissionsAsync: function (): Promise<void> { throw new Error("Function not implemented."); }
+// };
 
 describe('test default response when NOT defined in the environment variables', function () {
 
@@ -22,31 +25,38 @@ describe('test default response when NOT defined in the environment variables', 
 
     const webhook = new Webhook(environmentVariables, publisher);
 
-    const request = sinon.mock(http.IncomingMessage);
+    const request = sinon.stubInterface<IncomingMessage>();
     request.headers = {
         "content-type": "application/json"
     };
-    request.on = sinon.fake();
-    const response = sinon.mock(http.ServerResponse);
+    // request.on = sinon.fake();
+    // const response = sinon.mock(http.ServerResponse);
+    const response = sinon.stubInterface<ServerResponse>();
 
     //act
     webhook.handlerHttp1(request, response);
 
-    //assert
+    //assert 
     it('request on data', function () {
-        assert.equal(request.on.calledExactly(2), true);
-        // assert.equal(response.status.calledOnceWith(200), true);
+        assert.equal(request.on.callCount, 2);
+        const dataListener = request.on.getCalls().find(c => c.firstArg === 'data');
+        assert.equal(dataListener.firstArg, 'data');
+        //call listener function for 'data' event
+        dataListener.lastArg(Buffer.from('test'));
+        const endListener = request.on.getCalls().find(c => c.firstArg === 'end');
+        assert.equal(endListener.firstArg, 'end');
+        //call listener function for 'end' event
+        endListener.lastArg();
     });
 
-
     it('statusCode', function () {
-        // assert.equal(response., true);
-        // assert.equal(response.status.calledOnceWith(200), true);
+        assert.equal(response.writeHead.callCount, 1);
+        assert.equal(response.writeHead.getCall(0).firstArg, 200);
     });
 
     it('response body', function () {
-        assert.equal(response.end.calledOnce, true);
-        // assert.equal(response.end.calledOnceWith("OK"), true);
+        assert.equal(response.end.callCount, 1);
+        assert.equal(response.end.getCall(0).firstArg, 'OK');
     });
 
 });
